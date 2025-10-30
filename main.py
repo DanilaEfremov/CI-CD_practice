b803f7657804238187e664c04f2b8fb774ee36a5
Ефремов Данила <mr.dan.efremov@yandex.ru>
2025-10-30 18:25:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    """Простая функция сложения двух чисел"""
+    return a + b
+
+
+if __name__ == "__main__":
+    x = 5
+    y = 3
+    print(f"{x} + {y} = {add_numbers(x, y)}")
