4da82bfe9a1cd041b42283660d3b9f641dda5a68
Ефремов Данила <mr.dan.efremov@yandex.ru>
2025-10-30 21:43:51 +0300

    added multiply

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def add_numbers(a, b):
     """Простая функция сложения двух чисел"""
     return a + b
 
+def multiply_numbers(a, b):
+    """Простая функция умножения двух чисел"""
+    return a + b
+
 
 if __name__ == "__main__":
     x = 5
     y = 3
     print(f"{x} + {y} = {add_numbers(x, y)}")
+
